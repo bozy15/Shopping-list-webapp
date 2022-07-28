4fb301acf711cd35ee62fe8581382f74ffbeffc0
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2022-07-28 22:43:50 +0100

    Add edit/add/toggle functionality

--- list/urls.py
+++ list/urls.py
@@ -4,4 +4,6 @@ from list import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("add_item", views.add_item, name="add_item"),
+    path("edit_item/<item_id>", views.edit_item, name="edit_item"),
+    path("toggle_item/<item_id>", views.toggle_item, name="toggle_item"),
 ]
--- list/views.py
+++ list/views.py
@@ -1,5 +1,4 @@
-from multiprocessing import context
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -10,6 +9,7 @@ def index(request):
     # Render the template with the items
     context = {"items": items}
     return render(request, "list/shopping_list.html", context)
+
 
 # View to add an item to the list
 def add_item(request):
@@ -25,6 +25,35 @@ def add_item(request):
     # Create an instance of the form
     # and render it to the template
     form = ItemForm()
-    context = {'form': form}
+    context = {"form": form}
     return render(request, "list/add_item.html", context)
-    
+
+
+# View to edit an item in the list
+def edit_item(request, item_id):
+    # Get item or return 404 error
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == "POST":
+        # Create a form instance and populate it with data from the request
+        form = ItemForm(request.POST, instance=item)
+        # Check if the form is valid:
+        if form.is_valid():
+            # Save the new item to the database
+            # and redirect to the list page
+            form.save()
+            return redirect(index)
+    # Create an instance of the form
+    # and render it to the template
+    form = ItemForm(instance=item)
+    context = {"form": form}
+    return render(request, "list/edit_item.html", context)
+
+# View to toggle an item's "completed" status
+def toggle_item(request, item_id):
+    # Get item or return 404 error
+    item = get_object_or_404(Item, id=item_id)
+    # Toggle the item's "completed" status
+    item.complete = not item.complete
+    item.save()
+    # Redirect to the list page
+    return redirect(index)
